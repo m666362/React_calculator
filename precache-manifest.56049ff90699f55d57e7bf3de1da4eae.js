729bf09cc19cb48803737d208de719566944dc9f
m666362 <mdrayhan9464@gmail.com>
2021-06-23 18:33:58 +0600

    Updates

---
+++ precache-manifest.56049ff90699f55d57e7bf3de1da4eae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b150d6cfe6558b5b8eccae67114c717",
+    "url": "/React_calculator/index.html"
+  },
+  {
+    "revision": "4a4078883bf29bc3a23a",
+    "url": "/React_calculator/static/css/main.f8ae2ee4.chunk.css"
+  },
+  {
+    "revision": "b995ff3f966cd4da4d47",
+    "url": "/React_calculator/static/js/2.3a3d70c7.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/React_calculator/static/js/2.3a3d70c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4078883bf29bc3a23a",
+    "url": "/React_calculator/static/js/main.9c69a9ee.chunk.js"
+  },
+  {
+    "revision": "a3b335432d385c78b83c",
+    "url": "/React_calculator/static/js/runtime-main.da160a55.js"
+  }
+]);
